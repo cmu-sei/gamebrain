71e20aaf1e78a665808afdf37fcdd628245fb604
Joe Vessella <jvessella@cert.org>
2022-03-17 17:19:06 -0400

    Header proof of concept.

--- gamebrain/test.py
+++ gamebrain/test.py
@@ -1,8 +1,17 @@
-import gbsettings
+from typing import Optional
 
+from fastapi import FastAPI, Header
 from oauthlib.oauth2 import BackendApplicationClient
+from pydantic import BaseModel
+import requests
 from requests_oauthlib import OAuth2Session
 import yaml
+
+import gbsettings
+
+
+class User(BaseModel):
+    user_id: str
 
 
 def main():
@@ -11,14 +20,22 @@ def main():
     identity = settings.identity
     token_url = f'{identity.base_url}{identity.token_endpoint}'
 
-    client = BackendApplicationClient(client_id=identity.client_id)
-    session = OAuth2Session(client=client)
-    token = session.fetch_token(token_url=token_url, client_id=identity.client_id, client_secret=identity.client_secret, verify=settings.ca_cert_path)
+    JWKS = requests.get(f"{identity.base_url}.well-known/openid-configuration/jwks", verify=settings.ca_cert_path)
 
-    print(token)
+    # client = BackendApplicationClient(client_id=identity.client_id)
+    # session = OAuth2Session(client=client)
+    # token = session.fetch_token(token_url=token_url, client_id=identity.client_id, client_secret=identity.client_secret, verify=settings.ca_cert_path)
 
-    r = session.get("https://foundry.local/topomojo/api/gamespaces")
-    print(r.status_code)
+    # print(token)
+
+    # r = session.get("https://foundry.local/topomojo/api/gamespaces")
+    # print(r.status_code)
+
+app = FastAPI()
+
+@app.get("/")
+async def header_test(authorization: str = Header(None)):
+    print(authorization)
 
 
 if __name__ == "__main__":
