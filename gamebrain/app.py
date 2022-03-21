4ee0efa304331b4412a66dc0f08325e80226b851
Joe Vessella <jvessella@cert.org>
2022-03-21 16:40:57 -0400

    Formatting and added a simple Oauth2 request.

--- gamebrain/app.py
+++ gamebrain/app.py
@@ -1,9 +1,10 @@
 from typing import Optional
 from urllib.parse import urljoin
 
-from fastapi import FastAPI, Security
+from fastapi import FastAPI, HTTPException, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from jose import jwt
+from oauthlib.oauth2 import LegacyApplicationClient
 import requests
 from requests_oauthlib import OAuth2Session
 import yaml
@@ -13,27 +14,42 @@ from .util import url_path_join
 
 
 SETTINGS = load_settings("./settings.yaml")
-JWKS = requests.get(url_path_join(SETTINGS.identity.base_url, SETTINGS.identity.jwks_endpoint), verify=SETTINGS.ca_cert_path).json()
 
-app = FastAPI()
-security = HTTPBearer()
+JWKS = requests.get(
+    url_path_join(SETTINGS.identity.base_url, SETTINGS.identity.jwks_endpoint),
+    verify=SETTINGS.ca_cert_path
+).json()
+
+OAUTH2_SESSION = OAuth2Session(client=LegacyApplicationClient(client_id=SETTINGS.identity.client_id))
+
+OAUTH2_SESSION.fetch_token(
+    token_url=url_path_join(SETTINGS.identity.base_url, SETTINGS.identity.token_endpoint),
+    username="administrator@foundry.local",
+    password="foundry",
+    client_id=SETTINGS.identity.client_id,
+    client_secret=SETTINGS.identity.client_secret,
+    verify=SETTINGS.ca_cert_path
+)
+
+APP = FastAPI()
 
 
 def check_jwt(token: str):
-    print(token)
-    payload = jwt.decode(token, JWKS, audience=SETTINGS.identity.jwt_audience, issuer=SETTINGS.identity.jwt_issuer)
-    return payload
-
-@app.get("/authtest")
-async def header_test(auth: HTTPAuthorizationCredentials = Security(security)):
     try:
-        payload = check_jwt(auth.credentials)
-    except Exception as e:
-        print(e)
-    else:
-        print(payload)
+        return jwt.decode(token, JWKS, audience=SETTINGS.identity.jwt_audience, issuer=SETTINGS.identity.jwt_issuer)
+    except (JWTError, JWTClaimsError, ExpiredSignatureError) as e:
+        raise HTTPException(status_code=401, detail="JWT Error")
+
+@APP.get("/authtest")
+async def auth_test(auth: HTTPAuthorizationCredentials = Security(HTTPBearer())):
+    payload = check_jwt(auth.credentials)
+    user_id = payload["sub"]
+
+    params = {"uid": user_id, "Filter": "collapse"}
+    player = OAUTH2_SESSION.get(
+        url_path_join(SETTINGS.gameboard.base_gb_url, SETTINGS.gameboard.players_endpoint),
+        verify=SETTINGS.ca_cert_path,
+        params=params
+    )
+
     return ""
-
-
-if __name__ == "__main__":
-    main()
