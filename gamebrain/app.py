2c8760828ef096ef4e36023b8d959a29a2f8d1fe
Joe Vessella <jvessella@cert.org>
2022-03-22 17:33:17 -0400

    Added calls to register a gamespace for a player.

--- gamebrain/app.py
+++ gamebrain/app.py
@@ -1,3 +1,4 @@
+import datetime
 from typing import Optional
 from urllib.parse import urljoin
 
@@ -47,7 +48,7 @@ async def auth_test(auth: HTTPAuthorizationCredentials = Security(HTTPBearer()))
 
     params = {"uid": user_id, "Filter": "collapse"}
     players = OAUTH2_SESSION.get(
-        url_path_join(SETTINGS.gameboard.base_gb_url, SETTINGS.gameboard.players_endpoint),
+        url_path_join(SETTINGS.gameboard.base_gb_url, "players"),
         verify=SETTINGS.ca_cert_path,
         params=params
     ).json()
@@ -59,19 +60,52 @@ async def auth_test(auth: HTTPAuthorizationCredentials = Security(HTTPBearer()))
         raise Exception("NYI")
 
     game_id = player["gameId"]
-    endpoint = SETTINGS.gameboard.specs_endpoint.format(game_id)
+    endpoint = f"game/{game_id}/specs"
     specs = OAUTH2_SESSION.get(
         url_path_join(SETTINGS.gameboard.base_gb_url, endpoint),
         verify=SETTINGS.ca_cert_path
     ).json()[0]
 
+    team_id = player["teamId"]
+    endpoint = f"team/{team_id}"
+    team = OAUTH2_SESSION.get(
+        url_path_join(SETTINGS.gameboard.base_gb_url, endpoint),
+        verify=SETTINGS.ca_cert_path
+    ).json()
+
     external_id = specs["externalId"]
-    endpoint = SETTINGS.topomojo.workspace_endpoint.format(external_id)
+    endpoint = f"workspace/{external_id}"
     workspace = OAUTH2_SESSION.get(
         url_path_join(SETTINGS.topomojo.base_api_url, endpoint),
         verify=SETTINGS.ca_cert_path
     ).json()
 
-    print(workspace)
+    expiration_time = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=1)
+    post_data = {
+        "resourceId": workspace["id"],
+        "graderKey": "",
+        "graderUrl": "",
+        "variant": 0,
+        "playerCount": len(team["members"]),
+        "maxAttempts": 3,
+        "maxMinutes": 60,
+        "points": 100,
+        "allowReset": True,
+        "allowPreview": True,
+        "startGamespace": True,
+        "expirationTime": expiration_time.isoformat(timespec="milliseconds").replace("+00:00", "Z"),
+        "players": [
+            {"subjectId": player["id"],
+             "subjectName": player["approvedName"]} for player in team["members"]
+        ]
+    }
+    endpoint = "gamespace"
+    response = OAUTH2_SESSION.post(
+        url_path_join(SETTINGS.topomojo.base_api_url, endpoint),
+        verify=SETTINGS.ca_cert_path,
+        json=post_data
+    )
+
+    print(response.json())
 
     return ""
