a44f5b844bb4a48bfa4db1d4f144e7dac47f745c
Joe Vessella <jvessella@cert.org>
2022-04-14 16:51:42 -0400

    Formatting.

--- gamebrain/util.py
+++ gamebrain/util.py
@@ -1,18 +1,20 @@
 from urllib.parse import urlsplit, urlunsplit
 
-
 """
 The following two functions yoinked from here:
 https://codereview.stackexchange.com/questions/13027/joining-url-path-components-intelligently
 
 Because urllib.parse.urljoin does some weird stuff.
 """
+
+
 def url_path_join(*parts):
     """Normalize url parts and join them with a slash."""
     schemes, netlocs, paths, queries, fragments = zip(*(urlsplit(part) for part in parts))
     scheme, netloc, query, fragment = first_of_each(schemes, netlocs, queries, fragments)
     path = '/'.join(x.strip('/') for x in paths if x)
     return urlunsplit((scheme, netloc, path, query, fragment))
+
 
 def first_of_each(*sequences):
     return (next((x for x in sequence if x), '') for sequence in sequences)
