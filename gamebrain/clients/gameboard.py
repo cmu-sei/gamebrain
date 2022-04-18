6126fa2f897fa185d8009e2ecbffae5a54531ce5
Joe Vessella <jvessella@cert.org>
2022-04-18 14:34:54 -0400

    Refactored gameboard calls same as topomojo calls.

--- gamebrain/clients/gameboard.py
+++ gamebrain/clients/gameboard.py
@@ -1,44 +1,35 @@
+from typing import Any, Dict, Optional
+import json as jsonlib
+
 from ..config import get_settings
-from ..util import url_path_join
-from .common import get_oauth2_session
+from .common import _service_get
 
 
-async def get_player_by_user_id(user_id: str, game_id: str):
-    settings = get_settings()
-    session = await get_oauth2_session()
+async def _gameboard_get(endpoint: str, query_params: Optional[Dict] = None) -> Optional[Any]:
+    resp = await _service_get(get_settings().gameboard.base_api_url, endpoint, query_params)
+    try:
+        return resp.json()
+    except jsonlib.JSONDecodeError:
+        return None
 
-    # This endpoint claims that it can accept query params, but "uid" appears not to work.
-    players = (await session.get(
-        url_path_join(settings.gameboard.base_api_url, "players")
-    )).json()
-    for player in players:
-        if player["userId"] == user_id and player["gameId"] == game_id:
-            return player
+
+async def get_player_by_user_id(user_id: str, game_id: str) -> Optional[Any]:
+    players = await _gameboard_get("players")
+    if players:
+        # This endpoint claims that it can accept query params, but "uid" appears not to work.
+        for player in players:
+            if player["userId"] == user_id and player["gameId"] == game_id:
+                return player
     return None
 
 
 async def get_game_specs(game_id: str):
-    settings = get_settings()
-    session = await get_oauth2_session()
-
-    return (await session.get(
-        url_path_join(settings.gameboard.base_api_url, f"game/{game_id}/specs")
-    )).json()
+    return await _gameboard_get(f"game/{game_id}/specs")
 
 
 async def get_team(team_id: str):
-    settings = get_settings()
-    session = await get_oauth2_session()
-
-    return (await session.get(
-        url_path_join(settings.gameboard.base_api_url, f"team/{team_id}")
-    )).json()
+    return await _gameboard_get(f"team/{team_id}")
 
 
 async def get_teams(game_id: str):
-    settings = get_settings()
-    session = await get_oauth2_session()
-
-    return (await session.get(
-        url_path_join(settings.gameboard.base_api_url, f"teams/{game_id}")
-    )).json()
+    return await _gameboard_get(f"teams/{game_id}")
