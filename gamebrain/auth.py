95d1f1d30f973fc1fdb87caf67c06f586d3e05dd
Joe Vessella <jvessella@cert.org>
2022-09-28 22:10:35 -0400

    Implemented JWT check to use scopes instead of audience.

--- gamebrain/auth.py
+++ gamebrain/auth.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Optional
 
 from fastapi import HTTPException
@@ -7,19 +8,26 @@ from jose.exceptions import JWTError, JWTClaimsError, ExpiredSignatureError
 from .config import get_settings, Global
 
 
-def check_jwt(token: str, audience: Optional[str] = None, require_sub: bool = False):
+def check_jwt(token: str, scope: Optional[str] = None, require_sub: bool = False):
     settings = get_settings()
     try:
-        return jwt.decode(
+        payload = jwt.decode(
             token,
             Global.get_jwks(),
-            audience=audience,
             issuer=settings.identity.jwt_issuer,
             options={
-                "require_aud": True,
+                "verify_aud": False,
                 "require_iss": True,
                 "require_sub": require_sub,
             },
         )
-    except (JWTError, JWTClaimsError, ExpiredSignatureError):
+        if scope:
+            token_scopes = payload.get("scope")
+            if not token_scopes:
+                raise JWTClaimsError(f"JWT Error. Required: {scope}. Provided: None. ")
+            if scope not in token_scopes:
+                raise JWTClaimsError(f"JWT Error. Required: {scope}. Provided: {token_scopes}.")
+        return payload
+    except (JWTError, JWTClaimsError, ExpiredSignatureError) as e:
+        logging.error(str(e))
         raise HTTPException(status_code=401, detail="JWT Error")
