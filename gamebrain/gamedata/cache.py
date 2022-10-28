a8324a780b7e2c5061be3e94babff1c714768be5
Joe Vessella <jvessella@cert.org>
2022-10-28 10:47:48 -0400

    Reverted earlier commit for now.

--- gamebrain/gamedata/cache.py
+++ gamebrain/gamedata/cache.py
@@ -461,12 +461,12 @@ class GameStateManager:
                         "task list that was not in the global task map: {task}."
                     )
                     continue
-                if active_comm_event and active_comm_event.commID == global_task.commID:
-                    logging.info(
-                        f"Attempted to mark task {global_task.taskID} complete for team {team_id}, "
-                        f"but they must complete comm event {active_comm_event.commID} first."
-                    )
-                    continue
+                # if active_comm_event and active_comm_event.commID == global_task.commID:
+                #     logging.info(
+                #         f"Attempted to mark task {global_task.taskID} complete for team {team_id}, "
+                #         f"but they must complete comm event {active_comm_event.commID} first."
+                #     )
+                #     continue
                 completion_criteria = global_task.markCompleteWhen
                 if not completion_criteria:
                     continue
