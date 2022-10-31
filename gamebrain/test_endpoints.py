a61a1eaf6871a7eeedea4133766d0bacc3a33e12
Joe Vessella <jvessella@cert.org>
2022-10-31 13:13:12 -0400

    Added test router endpoints to complete all task types.

--- gamebrain/test_endpoints.py
+++ gamebrain/test_endpoints.py
@@ -5,7 +5,18 @@ from fastapi import APIRouter, Depends, HTTPException
 from .auth import admin_api_key_dependency
 from .config import get_settings, SettingsModel
 from .clients import topomojo
-from .gamedata.cache import GameStateManager, GamespaceStateOutput, GameDataResponse
+from .gamedata.cache import (
+    GameStateManager,
+    GamespaceStateOutput,
+    GameDataResponse,
+    GenericResponse,
+    ScanResponse,
+    NonExistentTeam,
+    TeamID,
+    TaskID,
+    LocationID,
+    PowerMode,
+)
 
 
 test_router = APIRouter(
@@ -14,7 +25,9 @@ test_router = APIRouter(
 
 
 @test_router.get("/net_change/{gamespace_id}/{vm_name}/{network}")
-async def test_net_change(gamespace_id: str, vm_name: str, network: str):
+async def test_net_change(
+    gamespace_id: topomojo.GamespaceID, vm_name: str, network: str
+):
     gamespace_id = gamespace_id.strip()
     vm_name = vm_name.strip()
     network = network.strip()
@@ -51,16 +64,14 @@ async def test_net_change(gamespace_id: str, vm_name: str, network: str):
     await topomojo.change_vm_net(vm_id, network)
 
 
-@test_router.get("/mark_challenge_task_complete/{team_id}/{task_id}")
-async def test_mark_challenge_task_complete(
-    team_id: str, task_id: str
-) -> GameDataResponse:
+@test_router.get("/complete/challenge/{team_id}/{task_id}")
+async def test_complete_challenge(team_id: TeamID, task_id: TaskID) -> GameDataResponse:
     await GameStateManager.dispatch_challenge_task_complete(team_id, task_id)
     return await GameStateManager.get_team_data(team_id)
 
 
-@test_router.get("/mark_codexes_complete/{team_id}")
-async def test_mark_codexes_complete(team_id: str) -> GameDataResponse:
+@test_router.get("/complete/codex/{team_id}")
+async def test_complete_codex(team_id: TeamID) -> GameDataResponse:
     state_update = GamespaceStateOutput(
         exoArch="success",
         redRaider="success",
@@ -75,6 +86,58 @@ async def test_mark_codexes_complete(team_id: str) -> GameDataResponse:
     )
     await GameStateManager.dispatch_grading_task_update(team_id, state_update)
     return await GameStateManager.get_team_data(team_id)
+
+
+@test_router.get("/complete/comm/{team_id}")
+async def test_complete_comm(team_id: TeamID) -> GenericResponse:
+    try:
+        return await GameStateManager.complete_comm_event(team_id)
+    except NonExistentTeam:
+        raise HTTPException(status_code=404, detail="Team not found.")
+
+
+@test_router.get("/complete/jump/{location_id}/{team_id}")
+async def test_complete_jump(
+    location_id: LocationID, team_id: TeamID
+) -> GenericResponse:
+    try:
+        return await GameStateManager.jump(team_id, location_id)
+    except NonExistentTeam:
+        raise HTTPException(status_code=404, detail="Team not found.")
+
+
+@test_router.get("/complete/extendAntenna/{team_id}")
+async def test_complete_extend_antenna(team_id: TeamID) -> GenericResponse:
+    try:
+        return await GameStateManager.extend_antenna(team_id)
+    except NonExistentTeam:
+        raise HTTPException(status_code=404, detail="Team not found.")
+
+
+@test_router.get("/complete/retractAntenna/{team_id}")
+async def test_complete_retract_antenna(team_id: TeamID) -> GenericResponse:
+    try:
+        return await GameStateManager.retract_antenna(team_id)
+    except NonExistentTeam:
+        raise HTTPException(status_code=404, detail="Team not found.")
+
+
+@test_router.get("/complete/scan/{team_id}")
+async def test_complete_scan(team_id: TeamID) -> ScanResponse:
+    try:
+        return await GameStateManager.scan(team_id)
+    except NonExistentTeam:
+        raise HTTPException(status_code=404, detail="Team not found.")
+
+
+@test_router.get("/complete/power/{new_status}/{team_id}")
+async def test_complete_power_change(
+    new_status: PowerMode, team_id: TeamID
+) -> GenericResponse:
+    try:
+        return await GameStateManager.set_power_mode(team_id, new_status)
+    except NonExistentTeam:
+        raise HTTPException(status_code=404, detail="Team not found.")
 
 
 @test_router.get("/settings")
