3acc108f5258df724ea3443562db3efb2a496831
Joe Vessella <jvessella@cert.org>
2023-05-25 08:00:19 -0400

    Fill in associated models of GameEngineGameState.

--- gamebrain/clients/gameboardmodels.py
+++ gamebrain/clients/gameboardmodels.py
@@ -1,25 +1,65 @@
-from typing import Any
+from datetime import datetime
 
 from pydantic import BaseModel
 
 
-class GameEngineGameState(BaseModel):
-    id: str
-    name: str
-    managerId: str
-    managerName: str
-    markdown: str
-    audience: str
-    launchpointUrl: str
-    isActive: bool
+class GameEngineQuestionView(BaseModel):
+    answer: str | None
+    example: str | None
+    hint: str | None
+    isCorrect: bool
+    isGraded: bool
+    penalty: float
+    text: str | None
+    weight: float
 
-    players: list[Any]
-    vms: list[Any]
-    challenge: Any
-    # The following fields are .NET DateTimeOffset format, which does not
-    # automatically get parsed by datetime.datetime.fromisoformat(). It may
-    # be necessary to create a constructor to validate these if they get used.
-    whenCreated: str
-    startTime: str
-    endTime: str
-    expirationTime: str
+
+class GameEngineChallengeView(BaseModel):
+    text: str | None
+    maxPoints: int
+    maxAttempts: int
+    attempt: int
+    score: float
+    sectionCount: int
+    sectionIndex: int
+    sectionScore: float
+    sectionText:  str | None
+    lastScoreTime: datetime
+    questions: [GameEngineQuestionView] | None
+
+
+class GameEngineVmState(BaseModel):
+    id: str | None
+    name: str | None
+    isolationId: str | None
+    isRunning: bool
+    isVisible: bool
+
+
+class GameEnginePlayer(BaseModel):
+    gamespaceId: str | None
+    subjectId: str | None
+    subjectName: str | None
+    permission: str
+    isManager: bool
+
+
+class GameEngineGameState(BaseModel):
+    id: str | None
+    name: str | None
+    managerId: str | None
+    managerName: str | None
+    markdown: str | None
+    audience: str | None
+    launchpointUrl: str | None
+    isActive: bool
+    hasDeployedGamespace: bool
+
+    players: list[GameEnginePlayer] | None
+    vms: list[GameEngineVmState] | None
+    challenge: GameEngineChallengeView
+
+    whenCreated: datetime
+    startTime: datetime
+    endTime: datetime
+    expirationTime: datetime
