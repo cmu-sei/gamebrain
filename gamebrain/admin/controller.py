0c512d90876b92711cefd721bd9c5610d761b979
Joe Vessella <jvessella@cert.org>
2023-07-27 14:46:55 +0000

    Log deployment session time.

--- gamebrain/admin/controller.py
+++ gamebrain/admin/controller.py
@@ -231,6 +231,12 @@ async def _internal_deploy(deployment_data: Deployment):
 
     session_teams = []
 
+    logging.info(
+        f"Got start time {deployment_data.session.sessionBegin}, "
+        f"end time {deployment_data.session.sessionEnd}, and current "
+        f"time {deployment_data.session.now} from Gameboard."
+    )
+
     gamebrain_time = datetime.now(tz=timezone.utc)
     if abs(gamebrain_time - deployment_data.session.now).seconds > 2:
         logging.warning(
