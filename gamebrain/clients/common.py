e00499d63ff7c07c72dba5fb9f104b7e47192536
Joe Vessella <jvessella@cert.org>
2023-10-03 15:10:25 +0000

    Remove async with.

--- gamebrain/clients/common.py
+++ gamebrain/clients/common.py
@@ -38,31 +38,30 @@ class HttpMethod(Enum):
 async def _service_request_and_log(
     client: AsyncClient, method: HttpMethod, endpoint: str, data: dict[Any] = None
 ) -> Response:
-    async with client:
-        args = {
-            "method": method.value,
-            "url": endpoint,
-        }
-        if method in (HttpMethod.PUT, HttpMethod.POST):
-            args["json"] = data
-        elif method in (HttpMethod.GET,):
-            args["params"] = data
-        else:
-            raise ValueError("Unsupported HTTP method.")
+    args = {
+        "method": method.value,
+        "url": endpoint,
+    }
+    if method in (HttpMethod.PUT, HttpMethod.POST):
+        args["json"] = data
+    elif method in (HttpMethod.GET,):
+        args["params"] = data
+    else:
+        raise ValueError("Unsupported HTTP method.")
 
-        request = client.build_request(**args)
+    request = client.build_request(**args)
 
-        response = await client.send(request)
-        if not response.is_success:
-            api_key = request.headers.get('x-api-key')
-            if api_key:
-                request.headers['x-api-key'] = '<secret>'
-            error(
-                f"HTTP Request to {response.url} returned {response.status_code}\n"
-                f"HTTP Method was: {request.method}\n"
-                f"Headers were: {request.headers}\n"
-                f"Request Body was: {request.content}\n"
-                f"Response content was: {response.content}\n"
-            )
+    response = await client.send(request)
+    if not response.is_success:
+        api_key = request.headers.get('x-api-key')
+        if api_key:
+            request.headers['x-api-key'] = '<secret>'
+        error(
+            f"HTTP Request to {response.url} returned {response.status_code}\n"
+            f"HTTP Method was: {request.method}\n"
+            f"Headers were: {request.headers}\n"
+            f"Request Body was: {request.content}\n"
+            f"Response content was: {response.content}\n"
+        )
 
-        return response
+    return response
