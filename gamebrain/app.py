f798ed862dbbeb5bdc2b07c39d3fd1f6d262d854
Joe Vessella <jvessella@cert.org>
2024-03-08 13:30:31 -0500

    Remove credentials from exception handlers.

--- gamebrain/app.py
+++ gamebrain/app.py
@@ -96,17 +96,35 @@ APP = FastAPI(
 )
 
 
+def get_sanitized_headers(request: Request) -> dict:
+    # Starlette's Request Headers property is immutable,
+    # but I want to remove the API key from logs.
+    headers = dict(request.headers)
+    api_key = headers.get('x-api-key')
+    if api_key:
+        headers['x-api-key'] = '<secret>'
+    return headers
+
+
 @APP.exception_handler(HTTPException)
-async def debug_exception_handler(request: Request, exc: HTTPException):
-    logging.error(request.headers)
+async def debug_exception_handler(
+    request: Request,
+    exc: HTTPException
+):
+    headers = get_sanitized_headers(request)
+    logging.error(headers)
     return await http_exception_handler(request, exc)
 
 
 @APP.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
+async def validation_exception_handler(
+    request: Request,
+    exc: RequestValidationError
+):
+    headers = get_sanitized_headers(request)
     body = await request.json()
     logging.error(
-        f"Got invalid request headers: {request.headers} and body {body}")
+        f"Got invalid request headers: {headers} and body {body}")
     return await request_validation_exception_handler(request, exc)
 
 
